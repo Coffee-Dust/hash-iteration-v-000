1d129de6841366ccf18a50492406b76637ae8a1e
Jonathan Dust <d24dust@hotmail.com>
2018-02-02 17:16:31 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,6 +8,6 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    puts "#{kids_name} aren't you a little old for chuck e cheese?" if age > 16
+    #puts "#{kids_name} aren't you a little old for chuck e cheese?" if age > 16
   end
 end
